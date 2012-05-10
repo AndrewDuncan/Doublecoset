42b33f292beab829cd56de5b6f76568cf9b23302
Andrew Duncan <andrew.duncan@ncl.ac.uk>
2012-05-11 00:50:49 +0100

    mod 4 to 5 done

--- python/ex_K_f4-x-g-addlist.py
+++ python/ex_K_f4-x-g-addlist.py
@@ -79,35 +79,23 @@ G.addEdge(d,a,'c') #label x3
 G.addEdge(a,e,'b') #label x2
 G.addEdge(e,e,'c') #label x3
 
+Tree = {'5':'b','2':'A','3':'a','4':'C','1':''} #each vertex of G is associated to the path which  joins it to 1 in the spanning tree for G
+
+SimReps =[('3','1'),('1','3'),('3','4'),('2','4'),('3','5'),('1','4'),('2','5'),('5','3'),('4','1'),('4','2'),('4','3'),('5','2')]
 
 P=K.product(G)
 
 plist=P.vertices # list of vertices of P
-for c in plist:
-    print "(",str(c.label)[0:str(c.label).index('-')], ",", str(c.label)[str(c.label).index('-')+1:], ")",
-
-print  "\n --------------------------------\n"
+#for c in plist:
+#    print "(",str(c.label)[0:str(c.label).index('-')], ",", str(c.label)[str(c.label).index('-')+1:], ")",
+#
+#print  "\n --------------------------------\n"
 
 hitlist = []
 for c in plist:
     if str(c.label)[str(c.label).index('-')+1:] == '1':
         hitlist.append(c)
 #        print "(",str(c.label)[0:str(c.label).index('-')], ",", str(c.label)[str(c.label).index('-')+1:], ")",
-
-print hitlist
-#for i in range(len(plist)):
-#    for j in range(i+1,len(plist)):
-#        print  i, j,
-#print  "\n --------------------------------\n"
-
-#for c in plist:
-#    if str(c.label)[str(c.label).index('-')+1:] == '1':
-#        print str(c.inedges)
-#        print "\n ***\n"
-#        print str(c.inedgesList)
-#        print "\n ***************************\n"
-        
-
 
 
 def allneighbours(v): #return all edges incident to a given vertex
@@ -119,38 +107,9 @@ def allneighbours(v): #return all edges incident to a given vertex
         neighbourList.append((e,'out')) #append outedges
     return neighbourList
 
-v=0
-nl = allneighbours(plist[v])
-
-print "\n and now "
-print nl
-
-for i in range(len(nl)): #run through the list of neighbours of a  vertex
-    l=str(nl[i][0][1].label) #l is the label of the vertex
-    print l
-    left = l[0:l.index('-')]            #left is the lhs of the label: from theta-n
-    print left
-    right = l[l.index('-')+1:]          #right is the rhs of the label: from gamma-k
-    print right
-    L=str(nl[i][0][0])       #L is the edge label
-    d=str(nl[i][1])
-    if d == 'out':
-        Q = L.capitalize()              #Q is the label taking direction into account
-        print Q
-    else:
-        Q = L
-        print Q
 
 neighbours = dict()        
-Z=allneighbours(plist[0])
-n = str(plist[0].label)
-print "here is " 
-#neighbours[n]=[Z]
-#print neighbours
-#neighbours[n].append('dfd')
-#neighbours['2-2']=['aaa']
-#print neighbours
-#print n in neighbours 
+
 
 def indexneighbours(v): # make an index of all neighbours of a vertex
    n = str(v.label)
@@ -168,66 +127,40 @@ def onelist(): #make an index of all neighbours of vertics with rhs label = 1
 onelist()        
 #print neighbours
 
-v=plist[0]
-#print v.label in neighbours
-nl = neighbours[v.label]
-#print nl
-#print nl[0]
-for i in range(len(nl)): #run through the list of neighbours of a  vertex
-    l=str(nl[i][0][1].label) #l is the label of the vertex
-#    print l
-    left = l[0:l.index('-')]            #left is the lhs of the label: from theta-n
-#    print left
-    right = l[l.index('-')+1:]          #right is the rhs of the label: from gamma-k
-#    print right
-    L=str(nl[i][0][0])       #L is the edge label
-    d=str(nl[i][1])
-    if d == 'out':
-        Q = L.capitalize()              #Q is the label taking direction into account
-#        print Q
-    else:
-        Q = L
-#        print Q
-
+for a in range(len(hitlist)):
+    v=hitlist[a]
+    nl = neighbours[v.label]
+    vl = v.label[0:v.label.index('-')]            #left hand side of label of v
+    for i in range(len(nl)): #run through the list of neighbours of a  vertex
+        l=str(nl[i][0][1].label) #l is the label of the vertex
+        left = l[0:l.index('-')]            #left is the lhs of the label: from theta-n
+        right = l[l.index('-')+1:]          #right is the rhs of the label: from gamma-k
+        L=str(nl[i][0][0])       #L is the edge label
+        d=str(nl[i][1])
+        if d == 'out':
+            Q = L             #Q is the label taking direction into account
+        else:
+            Q = L.capitalize() 
     
-    for k in range(1,len(hitlist)): #run through the vertices in hitlist with bigger index than v
-        w = hitlist[k]              #w is the kth on the list
-        kl=  neighbours[w.label]    #kl is the list of vertices incident to w
-        for j in range(len(kl)): #run through the list of neighbours of   vertex w
-            kl_l = str(kl[j][0][1].label) #kl_l is the label of the jth vertex of kl
-            kleft = kl_l[0:kl_l.index('-')] #left is the lhs of the label of the jth vertex of kl
-            if left == kleft: #when left = kleft a path might be added to theta4
-                print "left is ", left, "kleft is", kleft
-#if left == 
-#       print "v is ", v.label, "l is",   l, "k is ", k, "kl is", kl
-         
-#len(Z)
-#for s in range(len(Z)):
-#    if not q in neighbours:
-#        neighbours[q]=['afa']
-#    else:
-#        neighbours[q].append('fff')
-
-#indexneighbours(plist[0])
-#      neighbours[q] = [n]
-# elif not n in neighbours[q]:
-#    neighbours[q].append(n)
-
-#    for i in range(len(nl)): #run through the list of neighbours 
-#       l=str(nl[i][0][1].label) #l is the label of the vertex i of the list
-#      left = l[0:l.index('-')]            #left is the lhs of the label: from theta-n
-#     right = l[l.index('-')+1:]          #right is the rhs of the label: from gamma-k
-#    L=str(nl[i][0][0])       #L is the edge label
-#   d=str(nl[i][1])
-# if d == 'out':
-#      Q = L.capitalize()              #Q is the label taking direction into account
-#  else:
-#     Q = L
-#neighbours(plist[1])
-           
-
-    
-#print "digraph P {"
-#print (str(P))
-#print "}"
-
+            for k in range(a+1,len(hitlist)): #run through the vertices in hitlist with bigger index than v
+                w = hitlist[k]              #w is the kth on the list
+                wl = w.label[0:w.label.index('-')]            #left hand side of label of w
+                kl=  neighbours[w.label]    #kl is the list of vertices incident to w
+                for j in range(len(kl)): #run through the list of neighbours of   vertex w
+                    kl_l = str(kl[j][0][1].label) #kl_l is the label of the jth vertex of kl
+                    kleft = kl_l[0:kl_l.index('-')] #left is the lhs of the label of the jth vertex of kl
+                    kright = kl_l[kl_l.index('-')+1:]          #right is the rhs of the label of the jth vertex
+                    if left == kleft: #when left = kleft a path might be added to theta4
+                        kl_L=str(kl[j][0][0])       #kl_L is the edge label
+                        kl_d=str(kl[j][1])
+                        if kl_d == 'out':
+                            kl_Q = kl_L              #kl_Q is the label taking direction into account
+                        else:
+                            kl_Q = kl_L.capitalize()
+                            
+                
+                        if Q == Tree[right] and kl_Q == Tree[kright]:#second condition for a path to be added
+                            if (right,kright) in SimReps: # final condition for a path to be added
+                                pass
+                            else:
+                                print "Add path for ", v.label, "to ", l, " and ", w.label, "to", kl_l, "labels in tree are", Q, kl_Q 
