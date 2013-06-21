11c6538b4d6bcabaa373d3a1006a31325448a89b
Michael John Gough <m.j.gough@newcastle.ac.uk>
2013-06-21 11:59:24 +0100

    fixes/updates

--- python/genfold/alg2a.py
+++ python/genfold/alg2a.py
@@ -1,27 +1,21 @@
 from alg1a import *
 
-def listsplitter(w,f1gens,f2gens,f1,f2): #Takes a word in F1*F2 and two sets of generators as input
+def listsplitter(w,f1gens,f2gens): #Takes a word in F1*F2 and two sets of generators as input, the new version (below) broke things
 	l=len(w)
 	ww=[] #ww is the "list of lists"
 	ww.append([w[0]])
 	if w[0] in f1gens:
 		j=1
-	elif w[0] in f2gens:
-		j=2
 	else:
-		print (w[0], 'isn\t in either free group')
-		j=0
+		j=2
 	for c in range(1,l):
-		if f1.is_element(c)==0 and f2.element()==0:
-			print (c, 'isn\t an element in either free group')
-			break
 		if j==1:
 				if w[c] in f1gens:
 					ww[-1]=ww[-1]+[w[c]]
 				else:
 					j=2
 					ww.append([w[c]])
-		elif j==2:
+		else:
 				if w[c] in f2gens:
 					ww[-1]=ww[-1]+[w[c]]
 				else:
@@ -34,23 +28,57 @@ def listsplitter(w,f1gens,f2gens,f1,f2): #Takes a word in F1*F2 and two sets of 
 	print(ww) #likely unnecessary but makes testing a lot easier
 	return(ww)
 
+#def listsplitter(w,f1gens,f2gens,f1,f2): #Takes a word in F1*F2 and two sets of generators as input
+#	l=len(w)
+#	ww=[] #ww is the "list of lists"
+#	ww.append([w[0]])
+#	if w[0] in f1gens:
+#		j=1
+#	elif w[0] in f2gens:
+#		j=2
+#	else:
+#		print (w[0], 'isn\t in either free group')
+#		j=0
+#	for c in range(1,l):
+#		if f1.is_element(w[c])==0 and f2.is_element(w[c])==0:
+#			print (w[c], 'isn\t an element in either free group')
+#			break
+#		if j==1:
+#				if w[c] in f1gens:
+#					ww[-1]=ww[-1]+[w[c]]
+#				else:
+#					j=2
+#					ww.append([w[c]])
+#		elif j==2:
+#				if w[c] in f2gens:
+#					ww[-1]=ww[-1]+[w[c]]
+#				else:
+#					j=1
+#					ww.append([w[c]])
+#	print (ww)
+#	for c in range(1,l):
+#		if w[c]==[]:
+#			w.remove(w[c])
+#	print(ww) #likely unnecessary but makes testing a lot easier
+#	return(ww)
+
 def reducelist(w):
 	for c in w: #freely reducing each fi in w
 		c = element(c)
 		w[c]=c.freely_reduce()
 	return(w)
 
-def nf_in_list(w,f1,f2,h1,h2):
-	h1.make_flower
-	h1.stallings
-	h2.make_flower
-	h2.stallings
+def nf_in_list(w,F1,F2,H1,H2):
+	H1.make_flower
+	H1.stallings
+	H2.make_flower
+	H2.stallings
 	
 	
-	S1 = h1.flower
+	S1 = H1.flower
 	D1 = S1.double()
 	DB1 = bfs(D1,sorted(D1.vertices, key=lambda pairs: [pairs.sortkey[1],pairs.sortkey[0]]))
-	S2 = h2.flower
+	S2 = H2.flower
 	D2 = S2.double()
 	DB2 = bfs(D2,sorted(D2.vertices, key=lambda pairs: [pairs.sortkey[1],pairs.sortkey[0]]))
 	
@@ -61,20 +89,20 @@ def nf_in_list(w,f1,f2,h1,h2):
 		s=2
 	for c in w[0:len(w)]:
 		if s==1:
-			c = tt=Normal_form(S1,c,DB1).spit_out_nf()
+			c = Normal_form(S1,c,DB1).spit_out_nf()
 			c = [c[3],c[1],c[4]]
 			s=2
 		else:
-			c = tt=Normal_form(S1,c,DB2).spit_out_nf()
+			c = Normal_form(S1,c,DB2).spit_out_nf()
 			c = [c[3],c[1],c[4]]
 			s=1
 	print(w)
 	return(w)
 
-def listtest(w,f1,f2): #w is a word, f1 and f2 free groups
+def listtest(w,F1,F2): #w is a word, F1 and F2 free groups
 	i=1
 	for c in range(1,len(w)):
-		if f1.is_element(w)==0 and f2.is_element(w)==0:
+		if F1.is_element(w)==0 and F2.is_element(w)==0:
 			print('c isn\'t an element of either free group')
 			i=0
 	return(i)
@@ -87,25 +115,30 @@ def alg2(w,f1,f2,h1,h2):
 	listtest(w,f1,f2)
 	w=listsplitter(w,f1gens,f2gens,f1,f2)
 	if w[0] in f1gens:
-		nf_in_list(w,f1,f2,h1,h2)
+		w=nf_in_list(w,f1,f2,h1,h2)
 	else:
-		nf_in_list(w,f1,f2,h1,h2)
+		w=nf_in_list(w,f1,f2,h1,h2)
+	w=joiner(w)
+	
 	print(w)
 	return(w)
 
-def joiner(w): #
+def joiner(w):
 	ww=[w[0][0]]
 	for i in range(0,len(w)-1):
 		ww.append(w[i][1])
-		t = w[i][2] + w[i+1][0]
-		ww.append(t)
+		t = [w[i][2]] + [w[i+1][0]]
+		c = element(t)
+		t = c.freely_reduce()
+		ww = ww + t
+		#ww.append(t)
 	ww.append(w[-1][1])
 	ww.append(w[-1][2])
 	print(ww)
 	return(ww)
 
-def quickreduce(w): #reduces only the necessary elements in dcnf
-		for i in range(0,len(w))
+def quickreduce(w): #reduces only the necessary elements in dcnf, not needed due to change to joiner function
+		for i in range(0,len(w)):
 			if w%2==0:
 				c = element(w[i])
 				w[i]=c.freely_reduce()
