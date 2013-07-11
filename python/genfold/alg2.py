c44bfe106d41fcc06700dca6e0ad08b5ea8a728e
Michael John Gough <m.j.gough@newcastle.ac.uk>
2013-07-11 10:18:29 +0100

    alg2

--- python/genfold/alg2.py
+++ python/genfold/alg2.py
@@ -141,3 +141,57 @@ def nf_in_list(w,flower1,flower2,double1,double2,F1,F2):
 
 def popper(w):
 	return [e for e in w if e!=""]
+
+from alg2 import *
+
+def amalgamate(w,F1,F2,H1,H2):
+	F=(F1.mongens,F2.mongens)
+	error=0
+	n=len(w)-1
+	if w[n] in F[0]:
+		f=0
+	else:
+		f=1
+	ff=f
+	for s in range(n,-1,-1):
+		if w[s] not in F[f]:
+			print("error message")
+			error=1
+			break
+	if error==1:
+		return
+	H=(H1,H2)
+	for s in range(n,-1,-1):
+		#w[s] in H[f]: #need to make this work - H[f] isn't actually a list of generators here, making a new hf_test function
+			#put w[s] in terms of H[1-f] here
+		t=hf_test(w[s],H[f])
+			if t[0]==1 #change form of w[s] below then indent everything necessary
+				w[s]=t[1]
+			if s==n:
+				w[s-1]=w[s-1]+w[s]
+				w[s]=element(w[s]).word
+				w.pop(s)
+			elif s==1:
+				w[s]=w[s]+w[s+1] #(reduced)
+				w[s]=element(w[s]).word
+				for i in range(s+2,n):
+					w[i-1]=w[i]
+					w.pop(len(w)-1)
+			else:
+				w[s-1]=w[s-1]+w[s]+w[s+1]
+				w[s-1]=element(w[s-1]).word
+				for i in range(s+2,n):
+					w[i-2]=w[i]
+					w.pop(len(w)-1)
+					w.pop(len(w)-1)
+			f=1-f
+	return(w)
+
+def hf_test(w,H):
+	t=graph_pass(H).acc_read_rem()
+	if len(t[1])==0 and len(t[2])==0
+		j=1
+		w=phi(H,t[4])
+	else:
+		(j,w)=(0,[])
+	return(j,w)
