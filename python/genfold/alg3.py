0dbe35b9e6c5c037d4b254746bd2fc6f1a774141
Michael John Gough <m.j.gough@newcastle.ac.uk>
2013-07-29 15:13:24 +0100

    alg3

--- python/genfold/alg3.py
+++ python/genfold/alg3.py
@@ -21,12 +21,15 @@ def d1(delta,F1,F2,Z):
 	delta_z=Graph(False,'Delta Z')
 	for k in (1,2):
 		delta_k0[k-1]=delta
-		#pseudocode starts here
-		for all_edges in delta_k0[k-1]: #part a
-			if the edge has a label in F[2-k]:
-				remove all_edges from delta_k0[k-1]
-		for all vertices in delta_k0[k-1]: #part b
-			if the vertex has only one edge:
+		for v in delta_k0[k-1].vertices: #part a
+			for outedges in v.outedgesList:
+				if the edge has a label in F[2-k]:
+					remove the outedge from the vertex
+			for inedges in v.inedgesList:
+				if the edge has a label in F[2-k]:
+					remove the inedge from the vertex
+		for v in delta_k0[k-1]: #part b
+			if len(v.inedgesList)+len(v.outedgesList)==1:
 				if the edge has a label from Z:
 					add edge to delta z
 					remove edge from F[k-1]
