dd944e614cc75bc93ca746840de1d6704eef5921
Michael John Gough <m.j.gough@newcastle.ac.uk>
2013-07-30 13:44:26 +0100

    alg3

--- python/genfold/alg3.py
+++ python/genfold/alg3.py
@@ -14,10 +14,10 @@ def alg3_pre():
 	else:
 		Z=free_group(F2.rank,'z')
 
-def d1(delta,F1,F2,Z):
+def d1(delta,F,Z):
 	delta_10=Graph(False,'Delta\' 1,0')
 	delta_20=Graph(False,'Delta\' 2,0')
-	delta_k0=(delta_10,delta_20)
+	delta_k0=[delta_10,delta_20)]
 	delta_z=Graph(False,'Delta Z')
 	for k in (1,2):
 		delta_k0[k-1]=delta
@@ -35,14 +35,24 @@ def d1(delta,F1,F2,Z):
 					if edge in Z:
 						#if k==1:
 						add edge to delta z ###
+						delta_z.addVertex(v.name)
 						if edge in v.inedgesList:
 							v.removeOutEdge(edge[0],edge[1])
+							#add inedge to z
 						elif edge in v.outedgesList:
 							v.removeInEdge(edge[0],edge[1])
+							#add outedge to z
+						
+						
 #		if k==1:
 #			delta_k0[1]=delta_k0[0]
 		for v in delta_k0[k-1]:
 			v.nu_im=('v') #part d
 			v.label='({0},{1})'.format(v.label,k) #part c
+	return delta_k0, delta_z
+
+def d2(delta_k0):
+	for k in delta_k0:
+		
 
 #lines labeled with ### need to be rewritten
