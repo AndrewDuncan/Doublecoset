c1ac7a05e47da13624edf26bf7b0e7d57768f340
Andrew Duncan <me@univ.ac.uk>
2013-10-16 23:43:43 +0100

    ?

---
+++ python/genfold/cex_display.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dot -Tpng cex/Delta_in.gv  |display &  
+dot -Tpng cex/flower1.gv   |display & 
+dot -Tpng cex/flower2.gv  |display & 
+dot -Tpng cex/D0_1.gv |display & 
+dot -Tpng cex/D0_2.gv   |display & 
+dot -Tpng cex/D0_Z.gv  |display &  
+dot -Tpng cex/D1_1.gv  |display &  
+dot -Tpng cex/D1_2.gv  |display & 
+dot -Tpng cex/P_1_1.gv |display & 
+dot -Tpng cex/P_1_2.gv|display & 
+dot -Tpng cex/D2_1.gv  |display & 
+dot -Tpng cex/D2_2.gv  |display & 
+dot -Tpng cex/D3_1.gv |display &   
+dot -Tpng cex/D3_2.gv |display &       
+
+
+
+
+
+
+
---
+++ python/genfold/diff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+diff alg3_test_D0_1.gv alg3_test/alg3_test_D0_1.gv
+diff alg3_test_D0_2.gv alg3_test/alg3_test_D0_2.gv
+diff alg3_test_D0_Z.gv alg3_test/alg3_test_D0_Z.gv
+diff alg3_test_D1_2.gv alg3_test/alg3_test_D1_2.gv
+diff alg3_test_flower1.gv alg3_test/alg3_test_flower1.gv
+diff alg3_test_D2_1.gv alg3_test/alg3_test_D2_1.gv   
+diff alg3_test_flower2.gv alg3_test/alg3_test_flower2.gv
+diff alg3_test_D2_2.gv alg3_test/alg3_test_D2_2.gv      
+diff alg3_test_P_1_1.gv alg3_test/alg3_test_P_1_1.gv
+diff alg3_test_D1_1.gv alg3_test/alg3_test_D1_1.gv  
+diff alg3_test_Delta_in.gv alg3_test/alg3_test_Delta_in.gv  
+diff alg3_test_P_1_2.gv alg3_test/alg3_test_P_1_2.gv
