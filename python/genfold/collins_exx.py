00e9959bda45c524a4f7dd5d26f87008bac8a4a9
Andrew Duncan <me@univ.ac.uk>
2014-01-26 23:43:56 +0000

    collins exx

---
+++ python/genfold/collins_exx.py
@@ -0,0 +1,83 @@
+from wordmap import *
+
+#set the name of  the output file
+outfile='mihailova/collins.txt'
+
+with open(outfile, "w") as out: #create outfile 
+    out.write("output file wordmap \n\n") #and write text to it
+#out.close()
+
+
+#Enter the number of generators of the group to be mapped 
+Grank=10
+
+#Input "Collins" generators (there must be Grank of them)
+a='x1'
+b='x2'
+c='x3'
+d='x4'
+e='x5'
+p='x6'
+q='x7'
+r='x8'
+t='x9'
+k='x10'
+
+#Enter the number of relators of the group to be mapped, and create a blank list of relators
+#(this makes typing easier below)
+num_rels=27
+R=[]
+for i in range(0,num_rels):
+    R.append([]) # the ith relator, initially blank
+
+print("len r", len(r))
+#Input relators: words in the free group on the "Collins generators:
+# as words in Collins generators
+R[0]=[(p,10),(a,1),(p,-1),(a,-1)]# the word p^10 a p^{-1} a^{-1}
+R[1]=[(p,10),(b,1),(p,-1),(b,-1)]# p^10 b p^{-1} b^{-1}
+R[2]=[(p,10),(c,1),(p,-1),(c,-1)]
+R[3]=[(p,10),(d,1),(p,-1),(d,-1)]
+R[4]=[(p,10),(e,1),(p,-1),(e,-1)]
+R[5]=[(a,1),(q,10),(a,-1),(q,-1)]
+R[6]=[(b,1),(q,10),(b,-1),(q,-1)]
+R[7]=[(c,1),(q,10),(c,-1),(q,-1)]
+R[8]=[(d,1),(q,10),(d,-1),(q,-1)]
+R[9]=[(e,1),(q,10),(e,-1),(q,-1)]
+R[10]=[(r,1),(a,1),(r,-1),(a,-1)]
+R[11]=[(r,1),(b,1),(r,-1),(b,-1)]
+R[12]=[(r,1),(c,1),(r,-1),(c,-1)]
+R[13]=[(r,1),(d,1),(r,-1),(d,-1)]
+R[14]=[(r,1),(e,1),(r,-1),(e,-1)]
+R[15]=[(p,1),(a,1),(c,1),(q,1),(r,1),(q,-1),(a,-1),(c,-1),(p,-1),(r,-1)]
+R[16]=[(p,2),(a,1),(d,1),(q,2),(r,1),(q,-2),(a,-1),(d,-1),(p,-2),(r,-1)]
+R[17]=[(p,3),(b,1),(c,1),(q,3),(r,1),(q,-3),(b,-1),(c,-1),(p,-3),(r,-1)]
+R[18]=[(p,4),(b,1),(d,1),(q,4),(r,1),(q,-4),(b,-1),(d,-1),(p,-4),(r,-1)]
+R[19]=[(p,5),(c,1),(e,1),(q,5),(r,1),(q,-5),(a,-1),(c,-1),(e,-1),(p,-5),(r,-1)]
+R[20]=[(p,6),(d,1),(e,1),(q,6),(r,1),(q,-6),(b,-1),(d,-1),(e,-1),(p,-6),(r,-1)]
+R[21]=[(p,7),(c,1),(d,1),(c,1),(q,7),(r,1),(q,-7),(e,-1),(c,-1),(d,-1),(c,-1),(p,-7),(r,-1)]
+R[22]=[(p,8),(c,1),(a,1),(a,1),(a,1),(q,8),(r,1),(q,-8),(a,-1),(a,-1),(a,-1),(p,-8),(r,-1)]
+R[23]=[(p,9),(d,1),(a,1),(a,1),(a,1),(q,9),(r,1),(q,-9),(a,-1),(a,-1),(a,-1),(p,-9),(r,-1)]
+R[24]=[(p,1),(t,1),(p,-1),(t,-1)]
+R[25]=[(q,1),(t,1),(q,-1),(t,-1)]
+R[26]=[(k,1),(a,-3),(t,1),(a,3),(k,-1),(a,-3),(t,-1),(a,3)]
+
+
+#if required make a sublist of  relators to be processed
+#List_rels=R[i:j] will process relators R[i] through to R[j] 
+
+#List_rels=R[0:8]# to list all relators replace this line with 
+List_rels=R
+i=0
+for R in List_rels:
+    print(i," ", R,"\n\n")
+    i+=1
+
+#now call the function map_to_two_gens, in the file wordmap.py
+#to translate the elements of List_rels into elements of F(a,b)
+#... the output appears in outfile
+######################
+im_Grels,Trels=map_to_two_gens(Grank,List_rels,outfile)
+##################
+
+### at the end close the output file
+out.close()
---
+++ python/genfold/wordmap.py
@@ -0,0 +1,142 @@
+from main_loop import *
+
+# #################################
+# function to:
+# make inverses of generators x1, ...
+def invert(g):
+    G=g[0].upper()+g[1::]
+    return(G)
+
+# #################################
+# function to:
+# expand words input using pairs
+
+def expand_word(w):
+    ex_w=[]
+    for x in w:
+        if int(x[1])>0:
+            for i in range(0,int(x[1])):
+                ex_w.append(x[0])
+        else:
+            G=invert(x[0])
+            for i in range(x[1],0):
+                ex_w.append(G)
+
+    return(ex_w)
+# #################################
+# function to:
+# make image words a^-i b a^i, for i = 0 to n-1
+def make_im(n):
+    Im=[]
+    Im_inv=[]
+    for i in range(0,n):
+        Im.append(['b'])
+        for k in range(0,i):
+            Im[i].append('a')
+            Im[i].insert(0,'A')
+
+        Im_inv.append(copy.deepcopy(Im[i]))
+        Im_inv[i][i]='B'
+        
+    return(Im,Im_inv)
+
+# ###############################
+# function to:
+# take a word in F(x1,...) and map it to a word in F(a,b) by
+# mapping generator xi to a^{-i+1} b a^{i-1}, for i=1,... ,n
+def morph(F,w,images,IMAGES):
+    W=[]
+    for x in w:
+        i=F.gens.index(x.lower())
+        if x==x.lower():
+            W.extend(images[i])
+        else:
+            W.extend(IMAGES[i])
+            
+    W=element(W).word
+    
+    return(W)
+
+# ###############################
+# function to:
+# #write a word in latex format 
+def word_to_tex(w):
+    S=''
+    if w==[]:
+        S='1'
+    else:
+        current=w[0]
+        power=1
+        for x in range(1,len(w)):
+            if w[x]==current:
+                power=power+1
+            else:
+                if current==current.lower():
+                    if power==1:
+                        S=S+current
+                    else:
+                        S=S+current+'^{'+str(power)+'}'
+                else:
+                    S=S+current.lower()+'^{-'+str(power)+'}'
+                current=w[x]
+                power=1
+    if current==current.lower():
+        if power==1:
+            S=S+current
+        else:
+            S=S+current+'^{'+str(power)+'}'
+    else:
+        S=S+current.lower()+'^{-'+str(power)+'}'
+                
+
+    return(S)      
+###########################################################################
+# #here begins wordmap main function
+##########################################################################
+def map_to_two_gens(Grank,List_rels,outfile):        
+    #define the free group F on Grank generators x1, ...
+    F=free_group(Grank,"x")
+    
+    # ###############################################################
+    #
+    # expand the relators into the usual word format
+    Grels=[]
+    for r in List_rels:
+        R=expand_word(r)
+        Grels.append(R)
+    
+    # ###############################################################
+    # make all relators reduced 
+    # and 
+    # test that all elements of Grels are in F
+    for r in Grels:
+        r=element(r).word
+        words_in_F=generators_in_free_group(F,Grels)# this will be 0 if all elements of Grels are in F, and 1 otherwise
+        if words_in_F!=0: #if some of Grels are not in F: halt with an error message
+            error_message="the list of relators entered must only contain elements of the free group of rank "+str(Grank)
+            sys.exit(error_message)
+
+            
+    # ###################
+    (images,IMAGES)=make_im(Grank)
+    
+    im_Grels=[]
+    for r in Grels:
+        R=morph(F,r,images,IMAGES)
+        im_Grels.append(R)
+
+    Trels=[]
+    for R in im_Grels:
+        S=word_to_tex(R)
+        Trels.append(S)
+        #open output file
+        with open(outfile, "a") as out: #in append mode
+            out.write(str(R)+"\n\n") #and  write unformatted relators to it
+
+    for S in Trels:
+        with open(outfile, "a") as out: #in append mode
+            out.write(str(S)+"\n\n") #and  write formatted relators to it
+
+    return(im_Grels,Trels)
+
+
