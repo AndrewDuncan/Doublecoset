58dbd2c4745056d1586c00a1eee900654b85ccf3
Andrew Duncan <me@univ.ac.uk>
2014-02-20 11:21:26 +0000

    ?

--- python/genfold/ball.py
+++ python/genfold/ball.py
@@ -1,11 +1,11 @@
 from main_loop import *
-
+#import pickle 
 ###########################################################################
 # #here begins ball main function, which constructs the ball of radius R
 # #on the list L of words input ... it should be made to check these words are in F1*F2
 ##########################################################################
-def ball(R,L,F1,F2,Hrank,H1,H2,outfile):        
-    #make a list of elements of L and their inverses
+def ball(R,L,F1,F2,Hrank,H1,H2,logfile,outputfile):        
+    #make a list S of elements of L and their inverses
     S=[]
     for l in L:
         S.append(l)
@@ -13,7 +13,7 @@ def ball(R,L,F1,F2,Hrank,H1,H2,outfile):
         linv=element(l).inverse()
         S.append(linv)
     
-    #for each element s of S:  make a list S \ s^{-1} and add it to T 
+    #for each element s of S:  make a list of elements of S not equal to s^{-1} and add it to T 
     T=[]
     for s in S:
         s_list=[]
@@ -23,42 +23,54 @@ def ball(R,L,F1,F2,Hrank,H1,H2,outfile):
         
         T.append(s_list)
 
-    B=[]
-    sphere=[]
+    B=[]# B will be the list of spheres of radius 1 to R, of undreduced words
+    sphere=[]#make the sphere of length 1 words
     for s in S:
         sphere.append([s])
     
-    B.append(sphere)
-    #print("S is ", S)
-    #print("B -1 is ", B[-1])
-    for r in range(1,R):
+    B.append(sphere)#the first entry of B is the sphere of length one words (usually the generators of K and their inverses)
+    #construct successive spheres and append them to B
+    for r in range(1,R):#construct the sphere of radius r
         sphere=[]
-        for w in B[-1]:
-            w_last=w[-1]
-            #print("r is ", r," w is ",w, " w_last ",w_last)
-            ind=S.index(w_last)
-            #print("ind of w_last ",ind)
-            multipliers=T[ind]
-            #print("multipliers ",multipliers)
-            for m in multipliers:
+        for w in B[-1]:#for each element w of the sphere of radius r-1
+            w_last=w[-1]#find the element l of S with wich w ends
+            ind=S.index(w_last) # find the position ind of l in S
+            multipliers=T[ind]# find the list of elements not begining l^{-1}
+            for m in multipliers:# add m to w on the right, for each such and add the result to the sphere  
                 w_new=w+[m]
                 sphere.append(w_new)
-        B.append(sphere)
-        #for b in B:
-        #    for w in b:
-        #        print("w  is ",w,"\n")
-
-    for w in B[-1]:
-        wrd=[]
-        for l in w:
-            wrd=wrd+l
+        B.append(sphere) #add the sphere of radius r to B
+    
+    #make normal forms of the elements constructed
+   
+    B_nf=[]# the normal forms of elements of B
+    for b in B:# for each sphere b
+        C=[]
+        for w in b: #for each word in b 
+            wrd=[]#make the word into a word in the generators F1*F2
+            for l in w:
+                wrd=wrd+l
             
-        wrd=element(wrd).word
-        print("wrd is ",wrd,"\n")
-        if len(wrd)>0:
-            lw=listsplitter(wrd,F1.mongens,F2.mongens)
-            nw=amalgam_normal_form(lw,F1,F2,H1,H2)
-            print("  ",nw[1],"\n")
-    return(S,T,B)
+            wrd=element(wrd).word# freely reduce 
+            #print("wrd ", wrd,"\n")
+            nf_wrd=[]
+            if len(wrd)>0:#find the dc normal form of wrd
+                lw=listsplitter(wrd,F1.mongens,F2.mongens)
+                nw=amalgam_normal_form(lw,F1,F2,H1,H2)
+                for s in nw[1]:
+                    for q in s:
+                        if q!=[]:
+                            nf_wrd=nf_wrd+q
+            C.append(nf_wrd)
+            #print("nf_wrd ",nf_wrd,"\n")
+
+        #print("C = ", C)
+        B_nf.append(C)
+    
+
+    #save B_nf to outputfile
+    #pickle.dump(B_nf, open(outputfile, "wb" ) )
+
+    return(S,T,B,B_nf)
 
 
--- python/genfold/ball_test.py
+++ python/genfold/ball_test.py
@@ -1,4 +1,5 @@
 from ball import *
+import pickle 
 #So that each test creates a new set of graphs: set the prefix for all file names for your particular test here:
 #if this name is the name of a directory - then that directory must exist as a sub-directory of the home dir of this file
 # ... the log file will also have this prefix
@@ -8,6 +9,11 @@ testfile='ball/'
 logfile=testfile+'log.txt'
 with open(logfile, "w") as log: #create logfile 
     log.write("log file ball_test.py \n\n") #and write text to it
+
+
+#file to store results
+outputfile=testfile+'ball.txt'
+
 #log.close()
 #verbose is a list (currently of length 11) of 0's and 1's. List entries correspond to 
 #functions or files as below. When the corresponding entry is set to one the program (or file) will
@@ -79,18 +85,6 @@ g3=['y1','y1','y3','Y1','y2']
 Hgens2=[g1,g2,g3]
 
 #########################################################
-###put in stuff from main loop 
-FZ=free_group(int(Hrank),"z")        
-#
-# find the folding corresponding to the generators entered
-#
-H1=construct_required_folding(Hname1,Hgens1,testfile,F1,Hrank,verbose,FZ,change_tree,logfile)
-
-# find the folding corresponding to the generators entered
-#
-H2=construct_required_folding(Hname2,Hgens2,testfile,F2,Hrank,verbose,FZ,change_tree,logfile)
-#
-#########################################################
 #########  now enter K
 ######################
 #enter name of subgroup K
@@ -125,16 +119,66 @@ Kgens=[k1,k2,k3]
 (H1,H2,FZ)=construct_H_foldings(Hrank,Hname1,Hname2,Hgens1,Hgens2,testfile,F1,F2,verbose,logfile,change_tree)
 K=construct_K(H1,H2,FZ,testfile,F1,F2,words1,words2,Kname,Kgens,verbose,logfile)
 
-L=[['y1','y1'], ['x3','X2'],['x1','z1']]
-L1=[['a'],['b'],['c']]
-L2=[['y1','y1'], ['Y1','y2'],['Y2','Y1']]
+#L=[['y1','y1'], ['x3','X2'],['x1','z1']]
+#L1=[['a'],['b'],['c']]
+#L2=[['y1','y1'], ['Y1','y2'],['Y2','Y1']]
 
-R=1# radius of ball to generate
-S,T,B=ball(R,Kgens,F1,F2,Hrank,H1,H2,logfile)
+R=3# radius of ball to generate
+S,T,B,B_nf=ball(R,Kgens,F1,F2,Hrank,H1,H2,logfile,outputfile)
 
+#w=B_nf[0][0]
+#print(w)
+
+#input delta_n, saved by the previous run of K_fix.py
+graphfile='input_K/delta_n_save.txt'
+delta_n = pickle.load( open(graphfile, "rb" ) )
+root=delta_n.root
+for i in range(0,len(B_nf)):
+    for w in B_nf[i]:
+        test_w=graph_pass(delta_n,w,root).acc_read_rem()
+        if len(test_w[1])+len(test_w[2])!=0:
+            #        print("w in K", w)
+            #    else:
+            # print("w not accepted ", w,"\n")
+            #print("TA",test_w,"\n\n")
+            ind=B_nf[i].index(w)
+            print("i is ", i,"ind ",ind)
+            #B_word=B[i][ind]
+            #print("original word ",B_word)
+#       Apref=[] #this will be the max accepted prefix
+#        Rpref=[] #this will be the max readable prefix following Apref
+#        Apref_in_Z=[] #this will be Apref written in Z generators instead of X generators
+#        suffix=self.word # this will be the remainder of the word
+#        return(Apref,Rpref,suffix,u,Apref_in_Z)
 #print("S = ", S,"\n\n T is \n")
+i=0
+with open(logfile, "a") as log: #write to logfile 
+    for b in B:
+        log.write("list B: "+str(i)+"\n") 
+        for w in b:
+            log.write(str(w)+"\n")
+        i+=1
+i=0
+with open(logfile, "a") as log: #append to logfile 
+    for b in B_nf:
+        log.write("list B_nf: "+str(i)+"\n") 
+        for w in b:
+            log.write(str(w)) 
 
-#for t in T:
-#    print(t,"\n")
+print("B")
+for i in range(0,len(B)):
+    print(len(B[i]))
+print("B_nf")
+for i in range(0,len(B_nf)):
+    print(len(B_nf[i]))
+#with open(outputfile, 'r') as output:
+#    B_data = output.read()
 
-#print("\n\nB", B,"\n ..B[0]", B[-1])
+#for b in B_data:
+#    print(b,"\n\n\n")
+#pickle.dump(B_nf, open(outputfile, "wb" ) )
+#print("\n\nB_data ", B_data)
+#print("\n\n B_data[-1]", B_data[-1])
+
+print("B[2][10]", B[2][10])
+print("B_nf[2][10]", B_nf[2][10])
